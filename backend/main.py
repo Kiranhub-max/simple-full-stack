cdefd7e972e4ffaaa5977ada151f09e575f11172
kirannagarakanti <ki00300n@gmail.com>
2026-08-01 00:13:37 +0530

    full-stack boilerplate ready

--- backend/main.py
+++ backend/main.py
@@ -19,14 +19,9 @@ app.add_middleware(
 """post and get user"""
 @app.post("/login")
 def login(user: UserCreate):
-
-   
+    
 
     """post logic"""
-
-   
-
-
     
     db.add(new_user)
     db.commit()
@@ -37,6 +32,33 @@ def get_users():
     db = SessionLocal()
 
     """get logic"""
-
+    
     
     return users
+
+
+"""update user"""
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: UserCreate):
+    db = SessionLocal()
+
+    """update logic"""
+
+    db.commit()
+
+    return {"message": "User Updated Successfully"}
+
+
+
+
+"""delete user"""
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    db = SessionLocal()
+
+    """delete logic"""
+    db.commit()
+
+    return {"message": "User Deleted Successfully"}
